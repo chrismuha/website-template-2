51d0e8685928f611d7898d9f5bdab7bd919573bb
muhachris297 <muhachris297@gmail.com>
2025-01-11 18:22:19 -0500

    fixed alignment issues based on the active language and formatting rules.

--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,11 @@
 var loader = document.getElementById("preloader");
 
 window.addEventListener("load", function () {
-loader.style.display = "none";
+    loader.style.display = "none";
 })
 
 
-$('.a-nav-toggle, .menu-main a').on('click', function(){
+$('.a-nav-toggle, .menu-main a').on('click', function () {
     if ($('html').hasClass('body-menu-opened')) {
         $('html').removeClass('body-menu-opened').addClass('body-menu-close');
     } else {
